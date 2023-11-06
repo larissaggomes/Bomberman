6b12561bfface27367ddfc9774e6cb8cf0c030a7
larissaggomes <larissgomes92@gmail.com>
2023-11-06 15:03:48 -0400

    feat: adicionando layers

--- game.js
+++ game.js
@@ -6,19 +6,59 @@ kaboom({
     clearcolor: [0,0,0,1]
 })
 
+loadRoot('https://i.imgur.com/');
+
+loadSprite('wall-steel', 'EkleLlt.png');
+loadSprite('brick-red', 'C46n8aY.png');
+loadSprite('door', 'Ou9w4gH.png');
+loadSprite('kaboom', 'etY46bP.png');
+loadSprite('bg', 'qIXIczt.png');
+loadSprite('wall-gold', 'VtTXsgH.png');
+loadSprite('wall-wood', 'U751RRV.png');
+
+loadSprite('bomberman', 'T0xbHb8.png', {
+    anims:
+});
+
 scene('game', () => {
-    layers(['bg', 'obj', 'ul'], 'obj');
+    layers(['bg', 'obj', 'ui'], 'obj');
 
-    loadRoot('https://i.imgur.com/')
 
-    loadSprite('wall-steel', 'EkleLlt')
-    loadSprite('brick-red', 'EkleLlt')
-    loadSprite('door', 'EkleLlt')
-    loadSprite('kaboom', 'EkleLlt')
-    loadSprite('bg', 'EkleLlt')
-    loadSprite('wall-gold', 'EkleLlt')
-    loadSprite('wall-wood', 'EkleLlt')
+    const maps = [
+    [
+        'aaaaaaaaaaaaaa',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'a            a',
+        'aaaaaaaaaaaaaa'
+    ]
+    ]
 
+    const levelCfg = {
+        width: 26,
+        height: 26,
+        a: [sprite('wall-steel'), 'wall-steel', solid(), 'wall'],
+        z: [sprite('brick-red'), 'brick-red', solid(), 'wall'],
+        d: [sprite('brick-red'), 'wall-brick-dool', solid(), 'wall'],
+        b: [sprite('wall-gold'), 'wall-gold', solid(), 'wall'],
+        w: [sprite('brick-wood'), 'wall-brick', solid(), 'wall'],
+        p: [sprite('brick-wood'), 'wall-brick-dool', solid(), 'wall'],
+        t: [sprite('door'), 'door', 'wall'],
+    }
+
+    const gamelevel = addLevel(maps[a], levelCfg);
 })
 
 go('game');
